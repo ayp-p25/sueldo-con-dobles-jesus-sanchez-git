a914c1a0550b29d9e4d3d94a7077af187c284909
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-02-26 20:01:15 -0600

    First

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,15 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+A = float(input("Ingrese las horas trabajadas: \n"))
+B = float(input("Ingrese el sueldo por hora : \n"))
+ 
+if A>48:
+    HE = A -48
+    Pe = HE*(2*B)
+    HN = 48
+else:
+    HE = 0
+    Pe = 0
+    HN = A
+Total = (HN*B)+Pe
+print("El total de horas extra fueron : ", HE)
+print("Sueldo por horas extra : ", Pe)
+print("Su pago total fue : ", Total)
